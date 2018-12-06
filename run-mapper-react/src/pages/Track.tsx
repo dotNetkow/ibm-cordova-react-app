14d5e0e0779a51e7bc78ae1090c235487ea66476
Josh Thomas <jthoms1@gmail.com>
2018-12-06 16:50:45 -0600

    add cordova.

--- run-mapper-react/src/pages/Track.tsx
+++ run-mapper-react/src/pages/Track.tsx
@@ -12,10 +12,15 @@ type State = {
   startDateTime: Date | null;
   endDateTime: Date | null;
   coordinateList: Coordinate[];
-  distance: number | null
+  distance: number
 }
 
 class Home extends Component<Props, State> {
+  geolocator: any;
+  geolocation: any;
+  map: any;
+  mapRef: React.Ref<HTMLDivElement>;
+
   constructor(props: any) {
     super(props);
 
@@ -23,8 +28,10 @@ class Home extends Component<Props, State> {
       startDateTime: null,
       endDateTime: null,
       coordinateList: [],
-      distance: null
+      distance: 0
     }
+
+    this.mapRef = React.createRef();
 
     this.startTracking = this.startTracking.bind(this);
     this.stopTracking = this.stopTracking.bind(this);
@@ -32,10 +39,38 @@ class Home extends Component<Props, State> {
     this.reset = this.reset.bind(this);
   }
   startTracking() {
+    this.geolocator = navigator.geolocation.watchPosition((data: Position) => {
+      if (data.coords == null) {
+        return;
+      }
 
+      this.setState((prevState) => {
+        const lastCoordinate = prevState.coordinateList[prevState.coordinateList.length - 1];
+        const currentCoordinate: [number, number] = [data.coords.latitude, data.coords.longitude];
+        let distance = prevState.distance;
+        if (lastCoordinate) {
+          distance += calcHaversineDistance(lastCoordinate, currentCoordinate);
+        }
+        return {
+          ...prevState,
+          distance,
+          coordinateList: prevState.coordinateList.concat([currentCoordinate])
+        }
+      });
+
+      this.map.moveCamera({ zoom: 15, target: {
+        lat: data.coords.latitude,
+        lng: data.coords.longitude
+      }});
+
+
+    }, () => {}, { enableHighAccuracy: true });
+  }
+  componentDidMount() {
+    this.map = (window as any).plugin.google.maps.Map.getMap(this.mapRef);
   }
   stopTracking() {
-
+    navigator.geolocation.clearWatch(this.geolocator);
   }
   saveRun() {
     if (this.state.startDateTime && this.state.endDateTime && this.state.distance) {
@@ -51,6 +86,7 @@ class Home extends Component<Props, State> {
   reset() {
 
   }
+
   render() {
     const duration = (this.state.startDateTime && this.state.endDateTime) ?
         differenceInSeconds(this.state.startDateTime, this.state.endDateTime) :
@@ -59,7 +95,8 @@ class Home extends Component<Props, State> {
     return (
       <div>
         <h1>Track Run</h1>
-        <div>
+        <div ref={this.mapRef} style={{ height: '100%' }}></div>
+        <footer>
           <dl>
             <dt>
               {format(duration, 'mm:ss:SSS')}<br/>
@@ -74,7 +111,7 @@ class Home extends Component<Props, State> {
             <button onClick={this.saveRun}>Save</button>
             <button onClick={this.reset}>Reset</button>
           </div>
-        </div>
+        </footer>
       </div>
     );
   }
