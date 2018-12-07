2b5c0abf1aaf9dd3e29ee9e1f727d142ddcfcf3e
Josh Thomas <jthoms1@gmail.com>
2018-12-07 15:05:58 -0600

    Move build to www.

---
+++ run-mapper-react/platforms/ios/www/precache-manifest.231f1d7c4ef15a6874b94e74cc79a604.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4e4424a962d69b629d85",
+    "url": "/static/js/main.4e4424a9.chunk.js"
+  },
+  {
+    "revision": "f3007bf5143f1e0639b6",
+    "url": "/static/js/1.f3007bf5.chunk.js"
+  },
+  {
+    "revision": "4e4424a962d69b629d85",
+    "url": "/static/css/main.0329dcd8.chunk.css"
+  },
+  {
+    "revision": "a247f82ed7a86fa1e5f6da77fd05cce6",
+    "url": "/index.html"
+  }
+];
